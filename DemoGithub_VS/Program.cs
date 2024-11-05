f683dd48b52d3877f172b6e22ecc701d809e6de9
QuyetNguyen0802 <quyetnguyenmanh0802@gmail.com>
2024-11-06 06:49:17 +0700

    test_revert

--- DemoGithub_VS/Program.cs
+++ DemoGithub_VS/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! demo_push-one more modifition");
-Console.WriteLine("Hello, World! demo_push-one more modifition");
-Console.WriteLine("Hello, World! demo_push-one more modifition");
-Console.WriteLine("Hello, World! demo_push-one more modifition");
+MyDemo();
+void MyDemo()
+{
+    Console.WriteLine("hello");
+}
